23ea127625c951479d32013bf478a849effcb9cb
yashkumar-binmile <yash.kumar@binmile.com>
2026-08-25 15:36:52 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('AI DevOps Demo - Version 1');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Application running on port ${PORT}`);
+});
